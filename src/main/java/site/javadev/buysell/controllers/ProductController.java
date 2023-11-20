76ae9d86ecf27084f09de097a98e015b2636cac5
gemansipator <gemansipator@gmail.com>
2023-11-20 19:25:12 +0300

    товары хранятся локально и удаляются

--- src/main/java/site/javadev/buysell/controllers/ProductController.java
+++ src/main/java/site/javadev/buysell/controllers/ProductController.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import site.javadev.buysell.models.Product;
 import site.javadev.buysell.services.ProductService;
 
 @Controller
@@ -18,6 +21,23 @@ public class ProductController {
         model.addAttribute("products", productService.listProducts());
         return "products";
 
+    }
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable Long id, Model model){
+
+        model.addAttribute("product", productService.getProductById(id));
+        return "product-info";
+
+    }
+    @PostMapping("/product/create")
+    public String createProduct(Product product){
+    productService.saveProduct(product);
+    return "redirect:/";
+    }
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id){
+    productService.deleteProduct(id);
+    return "redirect:/";
     }
 
 }
