d3869bcba53e19713515bfb88e9dfe17dd825178
gemansipator <gemansipator@gmail.com>
2023-11-20 12:46:51 +0300

    Initial commit

---
+++ src/test/java/site/javadev/buysell/BuysellApplicationTests.java
@@ -0,0 +1,13 @@
+package site.javadev.buysell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuysellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
